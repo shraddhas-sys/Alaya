dc4c998aff1ffcc996469af21ce9f38d31653d8a
shraddhas-sys <shraddhabasnet28@gmail.com>
2026-02-23 21:41:37 +0545

    feat: add PlannerViewModelFactory for dependency injection

--- app/src/main/java/com/example/alaya/viewmodel/PlannerViewModelFactory.kt
+++ app/src/main/java/com/example/alaya/viewmodel/PlannerViewModelFactory.kt
@@ -1,4 +1,17 @@
 package com.example.alaya.viewmodel
 
-class PlannerViewModelFactory {
-}
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.alaya.repository.PlannerRepo
+
+@Suppress("UNCHECKED_CAST")
+class PlannerViewModelFactory (
+    private val repo: PlannerRepo
+) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(PlannerViewModel::class.java)) {
+                return PlannerViewModel(repo) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
