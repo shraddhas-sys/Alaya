dd488c5f60239f98529062b54a3c79ea9928b895
shraddhas-sys <shraddhabasnet28@gmail.com>
2026-02-23 21:28:43 +0545

    feat: add YogaPlanModel data class for yoga scheduling

--- app/src/main/java/com/example/alaya/model/YogaPlanModel.kt
+++ app/src/main/java/com/example/alaya/model/YogaPlanModel.kt
@@ -1,3 +1,11 @@
 package com.example.alaya.model
 
-data class YogaPlanModel()
+data class YogaPlanModel(
+    val id: String = "",        // Document ID from Firebase
+    val userId: String = "",    // The UID of the user who created it
+    val date: String = "",
+    val yogaType: String = "",
+    val duration: String = "",
+    val notes: String = ""
+)
+
