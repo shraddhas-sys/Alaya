5d5815cc6e16ef07af28bfa5141e8a78e6aa3ca0
shraddhas-sys <shraddhabasnet28@gmail.com>
2026-02-23 21:15:57 +0545

    feat: add LocalMealItem data model for meal tracking

--- app/src/main/java/com/example/alaya/model/LocalMealItem.kt
+++ app/src/main/java/com/example/alaya/model/LocalMealItem.kt
@@ -1,3 +1,9 @@
 package com.example.alaya.model
 
-data class LocalMealItem()
+data class LocalMealItem(
+    val id: String = "",
+    val name: String,
+    val type: String,
+    val protein: String
+)
+
