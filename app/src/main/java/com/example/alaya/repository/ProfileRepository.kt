5a9547f62a6e6a2b3e049b3165934bf065febe32
shraddhas-sys <shraddhabasnet28@gmail.com>
2026-02-25 00:48:33 +0545

    feat: implement ProfileRepository for user data management

--- app/src/main/java/com/example/alaya/repository/ProfileRepository.kt
+++ app/src/main/java/com/example/alaya/repository/ProfileRepository.kt
@@ -1,4 +1,8 @@
 package com.example.alaya.repository
 
+import com.example.alaya.model.User
+
 interface ProfileRepository {
+    fun getUserProfile(uid: String, onResult: (User?) -> Unit)
+    fun updateUsername(uid: String, newName: String, onComplete: (Boolean) -> Unit)
 }
