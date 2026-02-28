4ae34ee964b5c84dcfa0f141576f993c233e4124
shraddhas-sys <shraddhabasnet28@gmail.com>
2026-02-28 13:15:42 +0545

    refactor: finalize Planner repository and its implementation

--- app/src/main/java/com/example/alaya/repository/PlannerRepo.kt
+++ app/src/main/java/com/example/alaya/repository/PlannerRepo.kt
@@ -6,4 +6,5 @@ import kotlinx.coroutines.flow.Flow
 interface PlannerRepo {
     suspend fun addPlan(plan: YogaPlanModel, callback: (Boolean, String?) -> Unit)
     fun getPlans(userId: String): Flow<List<YogaPlanModel>>
+    suspend fun deletePlan(userId: String, planId: String, callback: (Boolean, String?) -> Unit)
 }
