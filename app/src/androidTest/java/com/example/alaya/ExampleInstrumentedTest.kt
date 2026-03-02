65d328787892afd24831cb8bab8ea00e5b77c880
shraddhas-sys <shraddhabasnet28@gmail.com>
2026-03-02 23:35:36 +0545

    test: finalize instrumented tests for UI components and context

--- app/src/androidTest/java/com/example/alaya/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/alaya/ExampleInstrumentedTest.kt
@@ -1,24 +1,55 @@
 package com.example.alaya
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class AuthAppInstrumentedTest {
+
+    @get:Rule
+    val composeRule = createAndroidComposeRule<MainActivity>()
+    // Login Ui test
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.alaya", appContext.packageName)
+    fun testEmptyLogin_showsErrorMessage() {
+        composeRule.onNodeWithTag("loginButton").performClick()
+        composeRule.waitForIdle()
+        composeRule.onNodeWithText("Empty fields").assertIsDisplayed()
+    }
+
+    @Test
+    fun testSuccessfulLogin_navigation() {
+        // input field part
+        composeRule.onNodeWithTag("emailField").performTextInput("test@gmail.com")
+        composeRule.onNodeWithTag("passwordField").performTextInput("password123")
+        composeRule.onNodeWithTag("loginButton").performClick()
+        composeRule.waitForIdle()
+        composeRule.onNodeWithText("Welcome Home").assertExists()
+    }
+
+    // Registration Ui tests
+
+    @Test
+    fun testRegisterForm_validationError() {
+        composeRule.onNodeWithTag("nameField").performTextInput("Meow User")
+        composeRule.onNodeWithTag("registerButton").performClick()
+        composeRule.waitForIdle()
+        composeRule.onNodeWithText("Fill all fields").assertIsDisplayed()
+    }
+
+    @Test
+    fun testNavigation_toForgotPassword() {
+        // forgot password link part
+        composeRule.onNodeWithTag("forgotPasswordLink").performClick()
+        composeRule.waitForIdle()
+        composeRule.onNodeWithText("Enter email").assertIsDisplayed()
     }
 }
